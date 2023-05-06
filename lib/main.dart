e95262a920dfc1eb1c6a159e482198ea13a30cf9
Faisal Choura <faisal.choura@gmail.com>
2023-05-06 14:49:34 +0200

    added side bar

--- lib/main.dart
+++ lib/main.dart
@@ -12,17 +12,43 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Categoriser categoriser = Categoriser();
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: MaterialButton(
-            child: const Text('load CSV'),
-            onPressed: () {
-              categoriser.categorise();
-            },
-          ),
-        ),
-      ),
+    return const MaterialApp(
+      home: Layout(),
+    );
+  }
+}
+
+class Layout extends StatelessWidget {
+  const Layout({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: const [SizedBox(width: 240, child: AppMenu())],
+    );
+  }
+}
+
+class AppMenu extends StatelessWidget {
+  const AppMenu({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    List<String> menuItems = ['Categories'];
+    return Scaffold(
+      appBar: AppBar(title: const Text('Categoriser')),
+      body: ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: menuItems.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              title: Text(menuItems[index]),
+            );
+          }),
     );
   }
 }
