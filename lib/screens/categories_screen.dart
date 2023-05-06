f1da41d058fb2c1f5e8d00f3521174cdc1ee001b
Faisal Choura <faisal.choura@gmail.com>
2023-05-06 20:28:42 +0200

    added field to create category

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,15 +1,78 @@
 import 'package:flutter/material.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({super.key});
 
   @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen> {
+  List<String> categories = ['Restaurants', 'Grocceries'];
+  bool showNewCategoryField = false;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Categories')),
-      body: const Center(
-        child: Text('Categories'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Categories'),
+          actions: [
+            IconButton(
+                onPressed: () => _openNewCategoryInput(),
+                icon: const Icon(Icons.add))
+          ],
+        ),
+        body: Row(
+          children: [
+            SizedBox(
+              width: 180,
+              child: ListView.builder(
+                itemCount: categories.length,
+                itemBuilder: ((context, index) {
+                  if (index == 0 && showNewCategoryField) {
+                    return Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: Row(
+                            children: [
+                              const Flexible(
+                                flex: 3,
+                                child: TextField(),
+                              ),
+                              Flexible(
+                                  flex: 1,
+                                  child: IconButton(
+                                      onPressed: () => _closeNewCategoryInput(),
+                                      icon: const Icon(Icons.close)))
+                            ],
+                          ),
+                        ),
+                        ListTile(
+                          title: Text(categories[index]),
+                        )
+                      ],
+                    );
+                  }
+                  return ListTile(
+                    title: Text(categories[index]),
+                  );
+                }),
+              ),
+            ),
+            Container(width: 0.5, color: Colors.grey),
+          ],
+        ));
+  }
+
+  _openNewCategoryInput() {
+    setState(() {
+      showNewCategoryField = true;
+    });
+  }
+
+  _closeNewCategoryInput() {
+    setState(() {
+      showNewCategoryField = false;
+    });
   }
 }
