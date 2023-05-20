ebacf6249ba320d204618379c8f3da687ae7ac75
Faisal Choura <faisal.choura@gmail.com>
2023-05-20 23:02:39 +0200

    created report use cases

--- lib/features/reports/domain/usecase/build_report_usecase.dart
+++ lib/features/reports/domain/usecase/build_report_usecase.dart
@@ -1,1 +1,14 @@
+import '../model/report.dart';
+import '../model/transaction.dart';
 
+class BuildReportUseCase {
+  Report execute(Map<String, List<Transaction>> categorisedTransactions) {
+    Report report = Report(0, 0, []);
+    for (var categoryName in categorisedTransactions.keys) {
+      ReportCategory category =
+          ReportCategory(categoryName, categorisedTransactions[categoryName]!);
+      report.categories.add(category);
+    }
+    return report;
+  }
+}
