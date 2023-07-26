3d49f063ded20ea5f45073e5e2fb1a7d470a0d7a
Faisal Choura <faisal.choura@gmail.com>
2023-07-26 19:02:54 +0200

    chucking is possible while reading csv

--- lib/features/csv_files/presentation/viewmodel/csv_files_viewmodel.dart
+++ lib/features/csv_files/presentation/viewmodel/csv_files_viewmodel.dart
@@ -4,6 +4,7 @@ import 'package:expense_categoriser/features/csv_files/domain/model/csv_file_dat
 import 'package:expense_categoriser/features/csv_files/domain/usecase/import_files_usecase.dart';
 import 'package:expense_categoriser/features/csv_files/domain/usecase/remove_file_usecase.dart';
 import 'package:expense_categoriser/features/reports/domain/domain_modulde.dart';
+import 'package:expense_categoriser/features/reports/domain/enum/csv_file_chunk_size_enum.dart';
 import 'package:expense_categoriser/features/reports/domain/usecase/convert_csv_file_usecase.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -48,13 +49,9 @@ class CsvFilesViewModel extends StateNotifier<AsyncValue<void>> {
     _removeFileUseCase.execute(fileData);
   }
 
-  Future<List<String>> getHeaderRow(CsvFileData file) async {
-    final data = await _convertCsvFileUseCase.execute([file]);
-    return data[0]![0].map((e) => e.toString()).toList();
-  }
-
   Future<HeaderFirstRowData> getHeaderAndFirstRow(CsvFileData file) async {
-    final data = await _convertCsvFileUseCase.execute([file]);
+    final data = await _convertCsvFileUseCase
+        .execute([file], CsvFileChunkSizeEnum.small);
     final headerRow = data[0]![0].map((e) => e.toString()).toList();
     final firstRow = data[0]![1].map((e) => e.toString()).toList();
     return HeaderFirstRowData(headerRow, firstRow);
