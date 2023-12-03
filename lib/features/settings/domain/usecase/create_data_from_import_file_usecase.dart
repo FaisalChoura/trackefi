057dd42473e06365e313fc44362e070e657578bf
Faisal Choura <faisal.choura@gmail.com>
2023-12-03 12:38:13 +0400

    added usecase to create categories from data

---
+++ lib/features/settings/domain/usecase/create_data_from_import_file_usecase.dart
@@ -0,0 +1,53 @@
+import 'package:Trackefi/features/categories/data/data_module.dart';
+import 'package:Trackefi/features/categories/domain/model/category.dart';
+import 'package:Trackefi/features/categories/domain/repository/categories_repository.dart';
+import 'package:Trackefi/features/settings/data/data_module.dart';
+import 'package:Trackefi/features/settings/domain/repository/import_settings_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class CreateDataFromImportFileUseCase {
+  final CategoriesRepository _categoriesRepository;
+  final ImportSettingsRepository _importSettingsRepository;
+  CreateDataFromImportFileUseCase(
+      this._categoriesRepository, this._importSettingsRepository);
+  Future<bool> execute(Map json) async {
+    if (!_checkJsonMapIsValid(json)) {
+      return false;
+    }
+    try {
+      if (json['categories']) {
+        final listOfCategoriesJson = (json['categories'] as List<Map>);
+        for (var categoryJson in listOfCategoriesJson) {
+          await _categoriesRepository
+              .putCategory(Category.fromJson(categoryJson));
+        }
+      }
+    } catch (e) {
+      return false;
+    }
+
+    return true;
+  }
+
+  bool _checkJsonMapIsValid(Map json) {
+    const propertiesToCheck = ['categories', 'importSettings'];
+
+    if (json.keys.isEmpty) {
+      return false;
+    }
+
+    for (var key in json.keys) {
+      if (!propertiesToCheck.contains(key)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
+
+final createDataFromImportFileUseCaseProvider =
+    Provider((ref) => CreateDataFromImportFileUseCase(
+          ref.watch(categoriesRepositoryProvider),
+          ref.watch(importSettingsRepositoryProivder),
+        ));
--- lib/features/settings/presentation/viewmodel/export_import_meta_data_viewmodel.dart
+++ lib/features/settings/presentation/viewmodel/export_import_meta_data_viewmodel.dart
@@ -1,4 +1,5 @@
 import 'package:Trackefi/features/settings/domain/model/import_settings.dart';
+import 'package:Trackefi/features/settings/domain/usecase/create_data_from_import_file_usecase.dart';
 import 'package:Trackefi/features/settings/domain/usecase/generate_all_meta_data_export_usecase.dart';
 import 'package:Trackefi/features/settings/domain/usecase/generate_file_from_data_usecase.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -7,9 +8,12 @@ class ExportImportMetaDataViewModel
     extends StateNotifier<List<CsvImportSettings>> {
   final GenerateAllMetaDataReportUseCase _generateAllMetaDataReportUseCase;
   final GenerateFileFromDataUseCase _generateFileFromDataUseCase;
+  final CreateDataFromImportFileUseCase _createDataFromImportFileUseCase;
   ExportImportMetaDataViewModel(
-      this._generateAllMetaDataReportUseCase, this._generateFileFromDataUseCase)
-      : super([]);
+    this._generateAllMetaDataReportUseCase,
+    this._generateFileFromDataUseCase,
+    this._createDataFromImportFileUseCase,
+  ) : super([]);
 
   Future<Map> generateJson() async {
     return await _generateAllMetaDataReportUseCase.execute();
@@ -18,10 +22,14 @@ class ExportImportMetaDataViewModel
   Future<bool> generateFileFromJson(Map json) async {
     return await _generateFileFromDataUseCase.execute(json);
   }
+
+  Future<bool> createDataFromFile(Map json) async {
+    return await _createDataFromImportFileUseCase.execute(json);
+  }
 }
 
-final exportImportMetaDataViewModelProvider =
-    StateNotifierProvider((ref) => ExportImportMetaDataViewModel(
-          ref.watch(generateAllMetaDataReportUseCaseProvider),
-          ref.watch(generateFileFromDataUseCaseProvider),
-        ));
+final exportImportMetaDataViewModelProvider = StateNotifierProvider((ref) =>
+    ExportImportMetaDataViewModel(
+        ref.watch(generateAllMetaDataReportUseCaseProvider),
+        ref.watch(generateFileFromDataUseCaseProvider),
+        ref.watch(createDataFromImportFileUseCaseProvider)));
