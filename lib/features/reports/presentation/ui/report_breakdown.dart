b93b08b2be52df5500e9a90860354bdc7164c5b0
Faisal Choura <faisal.choura@gmail.com>
2024-01-06 22:48:14 +0400

    added min height and width

--- lib/features/reports/presentation/ui/report_breakdown.dart
+++ lib/features/reports/presentation/ui/report_breakdown.dart
@@ -23,128 +23,125 @@ class ReportBreakdown extends StatelessWidget {
     final dateFrom = report.dateRangeFrom;
     final dateTo = report.dateRangeTo;
     return SizedBox(
-      height: 740,
+      height: MediaQuery.of(context).size.height < 1000
+          ? MediaQuery.of(context).size.height * 0.85
+          : MediaQuery.of(context).size.height * 0.9,
       child: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(46),
           child: Column(
             children: [
-              IntrinsicHeight(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Expanded(
-                      child: TrCard(
-                        child: Column(
-                          children: [
-                            const Text(
-                              'Expenses',
-                            ),
-                            const SizedBox(
-                              height: 8,
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.center,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: TrCard(
+                      child: Column(
+                        children: [
+                          const Text(
+                            'Expenses',
+                          ),
+                          const SizedBox(
+                            height: 8,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                report.expenses.toString(),
+                                style: const TextStyle(
+                                    fontSize: 32, fontWeight: FontWeight.bold),
+                              ),
+                              const SizedBox(
+                                width: 4,
+                              ),
+                              Text(
+                                report.currencyId,
+                                style: TextStyle(fontSize: 12),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 32,
+                  ),
+                  Expanded(
+                    child: TrCard(
+                      child: Column(
+                        children: [
+                          const Text(
+                            'Income',
+                          ),
+                          const SizedBox(
+                            height: 8,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                report.income.toString(),
+                                style: const TextStyle(
+                                    fontSize: 32, fontWeight: FontWeight.bold),
+                              ),
+                              const SizedBox(
+                                width: 4,
+                              ),
+                              Text(
+                                report.currencyId,
+                                style: TextStyle(fontSize: 12),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 32,
+                  ),
+                  Expanded(
+                    child: TrCard(
+                      child: Column(
+                        children: [
+                          const Text(
+                            'Created on',
+                          ),
+                          const SizedBox(
+                            height: 4,
+                          ),
+                          Text(
+                            TrHelpers.simpleDateFormatter(report.createdAt),
+                            style: const TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          if (dateTo != null && dateFrom != null)
+                            Column(
                               children: [
-                                Text(
-                                  report.expenses.toString(),
-                                  style: const TextStyle(
-                                      fontSize: 32,
-                                      fontWeight: FontWeight.bold),
+                                const SizedBox(
+                                  height: 8,
+                                ),
+                                const Text(
+                                  'Date Range',
                                 ),
                                 const SizedBox(
-                                  width: 4,
+                                  height: 4,
                                 ),
                                 Text(
-                                  report.currencyId,
-                                  style: TextStyle(fontSize: 12),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 32,
-                    ),
-                    Expanded(
-                      child: TrCard(
-                        child: Column(
-                          children: [
-                            const Text(
-                              'Income',
-                            ),
-                            const SizedBox(
-                              height: 8,
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Text(
-                                  report.income.toString(),
+                                  "${TrHelpers.simpleDateFormatter(dateFrom)} - ${TrHelpers.simpleDateFormatter(dateTo)}",
                                   style: const TextStyle(
-                                      fontSize: 32,
                                       fontWeight: FontWeight.bold),
                                 ),
-                                const SizedBox(
-                                  width: 4,
-                                ),
-                                Text(
-                                  report.currencyId,
-                                  style: TextStyle(fontSize: 12),
-                                ),
                               ],
-                            ),
-                          ],
-                        ),
+                            )
+                        ],
                       ),
                     ),
-                    const SizedBox(
-                      width: 32,
-                    ),
-                    Expanded(
-                      child: TrCard(
-                        child: Column(
-                          children: [
-                            const Text(
-                              'Created on',
-                            ),
-                            const SizedBox(
-                              height: 4,
-                            ),
-                            Text(
-                              TrHelpers.simpleDateFormatter(report.createdAt),
-                              style:
-                                  const TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            if (dateTo != null && dateFrom != null)
-                              Column(
-                                children: [
-                                  const SizedBox(
-                                    height: 8,
-                                  ),
-                                  const Text(
-                                    'Date Range',
-                                  ),
-                                  const SizedBox(
-                                    height: 4,
-                                  ),
-                                  Text(
-                                    "${TrHelpers.simpleDateFormatter(dateFrom)} - ${TrHelpers.simpleDateFormatter(dateTo)}",
-                                    style: const TextStyle(
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                ],
-                              )
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
               const SizedBox(
                 height: 32,
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+
 import 'package:Trackefi/core/presentation/themes/light_theme.dart';
 import 'package:Trackefi/core/presentation/ui/layout.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:window_manager/window_manager.dart';
 
 import 'core/data/database/database_manager.dart';
 
@@ -10,6 +13,13 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await DBManager.isntance.initDatabase();
   await dotenv.load(fileName: ".env");
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+  if (Platform.isWindows) {
+    WindowManager.instance.setMinimumSize(const Size(1500, 800));
+  }
+
   runApp(const ProviderScope(child: MainApp()));
 }
 
