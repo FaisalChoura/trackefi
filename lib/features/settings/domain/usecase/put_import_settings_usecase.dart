c31d109552ed0079d1c5c396f15e6c6f3541efcf
Faisal Choura <faisal.choura@gmail.com>
2024-01-11 19:26:35 +0400

    state management added to import settings

--- lib/features/settings/domain/usecase/put_import_settings_usecase.dart
+++ lib/features/settings/domain/usecase/put_import_settings_usecase.dart
@@ -1,19 +1,26 @@
 import 'package:Trackefi/features/settings/data/data_module.dart';
+import 'package:Trackefi/features/settings/data/repository/import_settings_list_store_repository_impl.dart';
 import 'package:Trackefi/features/settings/domain/model/import_settings.dart';
+import 'package:Trackefi/features/settings/domain/repository/import_settings_list_store_repository.dart';
 import 'package:Trackefi/features/settings/domain/repository/import_settings_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class PutImportSettingsUseCase {
-  PutImportSettingsUseCase(this._importSettingsRepository);
+  PutImportSettingsUseCase(
+      this._importSettingsRepository, this._importSettingsListStoreRepository);
 
   final ImportSettingsRepository _importSettingsRepository;
+  final ImportSettingListStoreRepository _importSettingsListStoreRepository;
 
   Future<CsvImportSettings?> execute(CsvImportSettings settings) async {
     final savedImportSetting =
         await _importSettingsRepository.putImportSettings(settings);
+    _importSettingsListStoreRepository.addImportSetting(settings);
     return savedImportSetting;
   }
 }
 
-final putImportSettingsUseCase = Provider((ref) =>
-    PutImportSettingsUseCase(ref.watch(importSettingsRepositoryProivder)));
+final putImportSettingsUseCase = Provider((ref) => PutImportSettingsUseCase(
+      ref.watch(importSettingsRepositoryProivder),
+      ref.watch(importSettingsListStoreRepositoryProvider),
+    ));
