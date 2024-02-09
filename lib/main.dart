37768d84c925ab451ab9a9028e95453360e1c2c7
Faisal Choura <faisal.choura@gmail.com>
2024-02-09 20:06:06 +0400

    enabled sentry

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,11 @@
 import 'dart:io';
 
-import 'package:Trackefi/core/domain/errors/error_object.dart';
 import 'package:Trackefi/core/presentation/themes/light_theme.dart';
-import 'package:Trackefi/core/presentation/ui/dialog.dart';
 import 'package:Trackefi/core/presentation/ui/layout.dart';
 import 'package:Trackefi/core/presentation/ui/update_checker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:rxdart/rxdart.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:window_manager/window_manager.dart';
 
@@ -22,16 +19,15 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await windowManager.ensureInitialized();
   WindowManager.instance.setMinimumSize(const Size(1500, 800));
-  // await SentryFlutter.init(
-  //   (options) {
-  //     options.dsn = dotenv.env['SENTRY_DSN'] ?? '';
-  //     // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
-  //     // We recommend adjusting this value in production.
-  //     options.tracesSampleRate = 1.0;
-  //   },
-  //   appRunner: () => runApp(const ProviderScope(child: MainApp())),
-  // );
-  runApp(const ProviderScope(child: MainApp()));
+  await SentryFlutter.init(
+    (options) {
+      options.dsn = dotenv.env['SENTRY_DSN'] ?? '';
+      // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+      // We recommend adjusting this value in production.
+      options.tracesSampleRate = 1.0;
+    },
+    appRunner: () => runApp(const ProviderScope(child: MainApp())),
+  );
 }
 
 class MainApp extends StatelessWidget {
