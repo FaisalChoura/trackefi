1da8a507a21c57f5a143f3a45b1a4e612dc07e4b
Faisal Choura <faisal.choura@gmail.com>
2024-02-10 13:51:16 +0400

    moved transaction and added curreny origin fields

---
+++ lib/features/reports/domain/model/transaction.dart
@@ -0,0 +1,26 @@
+import 'package:isar/isar.dart';
+import 'package:uuid/uuid.dart';
+
+@embedded
+class Transaction {
+  late String id;
+  String categorySnapshotId;
+  String name;
+  late DateTime date;
+  double amount;
+  double? originalAmount;
+  bool isIncome;
+  String currencyId;
+  String? originalCurrencyId;
+  Transaction({
+    this.name = '',
+    this.amount = 0,
+    String dateString = '1995-01-01',
+    this.isIncome = false,
+    this.currencyId = '',
+    this.categorySnapshotId = '',
+  }) {
+    id = const Uuid().v4();
+    date = DateTime.parse(dateString);
+  }
+}
